333849ce73eb477e76e1c79104bd9ba801b901f6
Eros Pacheco <devcoderys@gmail.com>
2026-02-06 17:47:54 +0000

    feat: crear tipo perfume casi terminado

--- scripts/bash/crearTipoPerfuma.sh
+++ scripts/bash/crearTipoPerfuma.sh
@@ -3,6 +3,7 @@
 clear
 path=$1
 salir=true
+crear=true
 
 if [ -z "$path" ]; then
     echo "Error: Debes especificar una ruta al ejecutar el script."
@@ -12,51 +13,64 @@ fi
 
 while $salir; do
     clear
+    crear=true
     echo "========= CREAR TIPO DE PERFUME ========="
     echo ""
     read -p "Indica el nombre para la nueva categoria de perfume (escribe q para salir) > " nombreTipoPerfume
 
-    if [  "$path/$nombreTipoPerfume" == "q" ]; then
+    if [  "$nombreTipoPerfume" == "q" ]; then
         clear
         echo "Has luego..."
-        sleep 5
-        salir=false
-        continue
+        sleep 3
+        exit 0
     fi
 
     if [ "$nombreTipoPerfume" == "/" ]; then
         clear
         echo -e "\nError: el nombre de la TipoPerfume no puede contener '/'...\n"
+        crear=false
         sleep 5
+        continue
     fi
 
     if [ -z "$nombreTipoPerfume" ]; then
         clear
         echo -e "\nError: El nombre de la categoría no puede estar vacío.\n"
+        crear=false
         sleep 5
+        continue
     fi
 
-    if [[ ! "$nombreTipoPerfume" =~ ^[a-zA-Z0-9] ]]; then
+    if [[ ! "$nombreTipoPerfume" =~ ^[a-zA-Z0-9]+$ ]]; then
         clear
         echo -e "\nError: El nombre contiene caracteres no válidos.\nSolo se permiten letras y números (sin espacios ni símbolos).\n"
+        crear=false
         sleep 5
+        continue
     fi
 
     if [ -d "$path/$nombreTipoPerfume" ]; then
         clear
-        echo -e "\nError: Ya existe una TipoPerfume con ese nombre.\n"
+        echo -e "\nError: Ya existe un tipo perfume ($nombreTipoPerfume) con ese nombre.\n"
+        crear=false
         sleep 5
+        continue
     fi
     
-    mkdir "$path/$nombreTipoPerfume"
-
-    if [ $? -eq 0 ]; then
-        chmod 777 "$path/$nombreTipoPerfume"
-        clear
-        echo -e "\nLa carpeta $nombreTipoPerfume se ha creado correctamente en $path\n"
-        salir=false
-    else
-        clear
-        echo -e "\nError: No se ha podido crear la carpeta $nombreTipoPerfume en $path\n"
+    if [ $crear = true ]; then
+        mkdir "$path/$nombreTipoPerfume"
+        if [ $? -eq 0 ]; then
+            chmod 777 "$path/$nombreTipoPerfume"
+            clear
+            echo -e "\nLa carpeta $nombreTipoPerfume se ha creado correctamente en $path\n"
+            salir=false
+            sleep 5
+        else
+            clear
+            echo -e "\nError: No se ha podido crear la carpeta $nombreTipoPerfume en $path\n"
+            sleep 5
+        fi
     fi
+
+    
 done
