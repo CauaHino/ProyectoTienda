538aee971cb4e8c2a970980d55e9244b29873cde
Eros Pacheco <devcoderys@gmail.com>
2026-02-07 18:23:36 +0000

    refactor: Cambiar como salimos del bucle porque la variable termina siendo redundante, otro bucle interno de confirmacion, cambio de donde se crea la vaiable 'crear'

--- scripts/bash/crearMarca.sh
+++ scripts/bash/crearMarca.sh
@@ -2,8 +2,6 @@
 
 clear
 path=$1
-salir=true
-crear=true
 
 if [ -z "$path" ] || [ ! -d "$path" ]; then
     echo "Error: Ruta de categoría no válida."
@@ -12,20 +10,21 @@ fi
 
 while true; do
     clear
+    crear=true
     echo "========= CREAR MARCA ========="
     echo ""
     echo "Categoría => $(basename "$path")"
     echo ""
-    read -p "Indica el nombre de la nueva marca (escribe s para salir) => " nuevaMarca
+    read -p "Indica el nombre de la nombre marca (escribe s para salir) => " nombreMarca
 
-    if [  "$path/$nuevaMarca" == "s" ]; then
+    if [  "$path/$nombreMarca" == "s" ]; then
         clear
         echo "Has luego..."
         sleep 3
         exit 0
     fi
 
-    if [ -z "$nuevaMarca" ]; then
+    if [ -z "$nombreMarca" ]; then
         clear
         echo -e "\nError: El nombre de la marca no puede estar vacío."
         sleep 5
@@ -34,7 +33,7 @@ while true; do
     fi
 
 
-    if [[ "$nuevaMarca" == .* ]]; then
+    if [[ "$nombreMarca" == .* ]]; then
         clear
         echo -e "\nError: El nombre no puede empezar por punto (.)."
         sleep 5
@@ -42,7 +41,7 @@ while true; do
         continue
     fi
 
-    if [[ "$nuevaMarca" == *"/"* ]]; then
+    if [[ "$nombreMarca" == *"/"* ]]; then
         clear
         echo -e "\nError: El nombre no puede contener el carácter '/'."
         sleep 5
@@ -50,7 +49,7 @@ while true; do
         continue
     fi
 
-    if [[ ! "$nuevaMarca" =~ ^[a-zA-Z0-9_-]+$ ]]; then
+    if [[ ! "$nombreMarca" =~ ^[a-zA-Z0-9_-]+$ ]]; then
         clear
         echo -e "\nError: Solo se permiten letras, números, '-' o '_'."
         sleep 5
@@ -59,7 +58,7 @@ while true; do
     fi
 
 
-    if [ -d "$RUTA_CATEGORIA/$nuevaMarca" ]; then
+    if [ -d "$path/$nombreMarca" ]; then
         clear
         echo -e "\nError: Ya existe esta marca ($nombreMarca) en esta categoria de perfumes.\n"
     fi
@@ -69,9 +68,26 @@ while true; do
         if [ $? -eq 0 ]; then
             chmod 777 "$path/$nombreMarca"
             clear
-            echo -e "\nLa carpeta $nombreMarca se ha creado correctamente en $path\n"
-            salir=false
-            sleep 5
+            echo -e "\nLa marca $nombreMarca se ha creado correctamente en $path\n" 
+            sleep 4
+
+            while true; do
+                clear
+                read -p "Desea seguir creando marcas? (pulse Enter para continuar o 's' para salir): " continuar
+
+                if [[ -z $continuar ]]; then
+                    break
+                elif [[ $continuar == "s" || $continuar == "S" ]]; then
+                    clear
+                    echo "Hasta luego..."
+                    sleep 3
+                    exit 0
+                else
+                    clear
+                    echo -e "\nIngrese la tecla indicada\n"
+                    sleep 3
+                fi                    
+            done
         else
             clear
             echo -e "\nError: No se ha podido crear la carpeta $nombreMarca en $path\n"
