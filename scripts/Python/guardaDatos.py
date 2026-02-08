6308b0d4f833dcab1ddea2e95c75bdb8455dac05
Eros Pacheco <devcoderys@gmail.com>
2026-02-08 07:33:17 +0000

    feat, fix: Importar libreria sleep para poder leer el error que lanzamos y luego comentar esa linea de datos que no esta en uso

--- scripts/Python/guardaDatos.py
+++ scripts/Python/guardaDatos.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from time import sleep
 
 ruta = sys.argv[1]
 nombre = sys.argv[2]
@@ -8,7 +9,7 @@ stock = sys.argv[4]
 ml = sys.argv[5]
 descripcion = sys.argv[6]
 
-datos = json.load(open(ruta))
+# datos = json.load(open(ruta))
 
 datos_nuevos = {
     "nombre": nombre,
@@ -17,9 +18,14 @@ datos_nuevos = {
     "ml": ml,
     "descripcion": descripcion
 }
+
 try:
     with open(ruta, "w") as archivo:
         json.dump(datos_nuevos, archivo, indent=4)
+except FileNotFoundError as e:
+    print(f"Error el archivo no ha podido ser encontrado: {e}")
+    sleep(4)
 except Exception as e:
     print(f"Error al guardar los datos: {e}")
+    sleep(4)
 
