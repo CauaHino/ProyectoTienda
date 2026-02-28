2d8e2543782ccf1a6ee6b8155a77f53988cfd7db
Eros Pacheco <devcoderys@gmail.com>
2026-02-28 20:55:48 +0000

    feat: Ver tienda terminado, el sistema de logs no lo veo necesario aqui

--- scripts/Python/verTienda.py
+++ scripts/Python/verTienda.py
@@ -1,8 +1,39 @@
 import curses
 from pathlib import Path
+import subprocess
 
 def listarTienda(terminal):
     ruta = Path("/tiendas/PerfumeriaPaco")
+    seleccion = 0
+    while True:
+        terminal.clear()
+        opciones = sorted([directorios for directorios in ruta.iterdir() if directorios.is_dir()])
+        opciones.append("Volver")
+
+        for i, opcion in enumerate(opciones):
+            if(opcion != "Volver"):
+                nombre = opcion.name
+            else:
+                nombre = opcion
+            if seleccion == i:
+                terminal.addstr(i + 2, 0, f"> {nombre}", curses.A_REVERSE)
+            else:
+                terminal.addstr(i + 2, 0, f"{nombre}")
+        
+        tecla = terminal.getch()
+
+
+        if tecla == curses.KEY_DOWN and seleccion < len(opciones) - 1:
+            seleccion += 1
+        elif tecla == curses.KEY_UP and seleccion > 0:
+            seleccion -= 1
+        elif tecla == ord('\n'):
+            if opciones[seleccion] == 'Volver':
+                return None
+            else:
+                listarMarca(terminal, opciones[seleccion])
+             
+def listarMarca(terminal, ruta):
     seleccion = 0
     while True:
         terminal.clear()
@@ -27,11 +58,40 @@ def listarTienda(terminal):
             seleccion += 1
         elif tecla == curses.KEY_UP and seleccion > 0:
             seleccion -= 1
-        elif tecla == ord('\n'): # Entra aqui si has pulsado la tecla ENTER
-            if seleccion == 0:
-                pass
-                
-# Cambiar la estructura para mostrar me gusta mas la del main
-# Hay que hacer:
-# 1. Funcion para listar las marcas, productos
-# 2. Posible escript para solo mostrar el contenido de los productos.
+        elif tecla == ord('\n'):
+            if opciones[seleccion] == 'Volver':
+                return None
+            else:
+                listarProducto(terminal, opciones[seleccion])
+
+def listarProducto(terminal, ruta):
+    seleccion = 0
+    while True:
+        terminal.clear()
+        terminal.addstr(0, 0, f"===== PERFUMERIA PACO: {ruta.name} =====")
+        opciones = sorted([archivos for archivos in ruta.iterdir() if archivos.is_file()])
+        opciones.append("Volver")
+
+        for i, opcion in enumerate(opciones):
+            if(opcion != "Volver"):
+                nombre = opcion.name
+            else:
+                nombre = opcion
+            if seleccion == i:
+                terminal.addstr(i + 2, 0, f"> {nombre}", curses.A_REVERSE)
+            else:
+                terminal.addstr(i + 2, 0, f"{nombre}")
+        
+        tecla = terminal.getch()
+
+        if tecla == curses.KEY_DOWN and seleccion < len(opciones) - 1:
+            seleccion += 1
+        elif tecla == curses.KEY_UP and seleccion > 0:
+            seleccion -= 1
+        elif tecla == ord('\n'):
+            if opciones[seleccion] == 'Volver':
+                return None
+            else:
+                terminal.clear()
+                curses.endwin()
+                subprocess.run( ["bash", "scripts/bash/verJsonTienda.sh", str(opciones[seleccion])] )
